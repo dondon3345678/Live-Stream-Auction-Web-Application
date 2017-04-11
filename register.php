d638e2a9aeb19909fe344a2c4b008f55b5741ae8
Mu Yu <muorange3@gmail.com>
2017-04-11 14:08:38 +0800

    Update register.php

--- register.php
+++ register.php
@@ -18,13 +18,6 @@
       
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
-      if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: welcome.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
+      header("location: welcome.php");
    }
 ?>
