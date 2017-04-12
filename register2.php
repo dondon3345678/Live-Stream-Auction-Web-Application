ab2dca45da70d2b7b5868192c226dba3c09d8aee
Mu Yu <muorange3@gmail.com>
2017-04-12 13:43:18 +0800

    Update register2.php

--- register2.php
+++ register2.php
@@ -1,32 +1,23 @@
 <?
 /** config **/
-
     $sqlserver = "localhost";
     $sqluser = "radius";
     $sqlpass = "AAAA0857";
     $db = "radius";
-
     $con = mysql_connect($sqlserver, $sqluser, $sqlpass);
-    //echo "before select db";
-    //exit();
     mysql_select_db($db, $con);
-    echo $_GET["res"];
-    if($_GET["res"] == "reg")
-    {
-	echo "if"; exit;
-	$username = $_GET['register_username'];
-        $password = $_GET['register_password'];
-	echo $username;
-	mysql_query("INSERT INTO radcheck (username, attribute, op, value) VALUES ($username,'User-Password', ':=', $password)");
-    mysql_query("INSERT INTO radusergroup (username, groupname) VALUES ($username, 'user')");
-    Header("Location: index.php");
-    }
 
+    if ($_GET["res"] == "reg")
+    {
+    	$username = $_GET['register_username'];
+            $password = $_GET['register_password'];
+    	echo $username;
+    	mysql_query("INSERT INTO radcheck (username, attribute, op, value) VALUES ($username,'User-Password', ':=', $password)");
+        mysql_query("INSERT INTO radusergroup (username, groupname) VALUES ($username, 'user')");
+        header("Location: index.php");
+    }
     else
     {
-	echo '<h1>Please input your username and password</h1><form name="form1" method="get" action="register.php?res=reg"><table><tr><th>Username</th><td><input type="text" name="register_username"></td></tr><tr><th>Password</th><td><input type="password" name="register_password"></td></tr></table><input type="submit" name="register" value="register"></form>'
+	   echo '<h1>Please input your username and password</h1><form name="form1" method="get" action="register.php"><table><tr><th>Username</th><td><input type="text" name="register_username"></td></tr><tr><th>Password</th><td><input type="password" name="register_password"></td></tr></table><input type="hidden" name="res" value="reg"><input type="submit" name="register" value="register"></form>';
     }
-
-
-
 ?>
